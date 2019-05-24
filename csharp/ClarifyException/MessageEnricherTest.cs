ea27d25e4365d88d88fb13a73a7d9a42e49f2225
george xiros <geo.xiros@gmail.com>
2019-05-24 07:36:35 +0300

    add some tests

--- csharp/ClarifyException/MessageEnricherTest.cs
+++ csharp/ClarifyException/MessageEnricherTest.cs
@@ -1,22 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace codingdojo
 {
     public class MessageEnricherTest
     {
-        [Fact]
-        public void SampleTest()
+        [Theory]
+        [InlineData("No matches found")]
+        [InlineData("Circular Reference xxx")]
+        [InlineData("Object reference not set to an instance of an object")]
+        [InlineData("Missing Formula")]
+        [InlineData("General Error")]
+        public void SimpleExceptionShouldReturnMessage(string message)
         {
             var enricher = new MessageEnricher();
 
             var worksheet = new SpreadsheetWorkbook();
 
-            var e = new Exception("Terrible problem");
+            var e = new Exception(message);
 
             var actual = enricher.EnrichError(worksheet, e);
 
-            Assert.Equal("Fixme", actual.Message);
+            Assert.Equal(message, actual.Message);
+        }
+        [Fact]
+        public void SpreadsheetExceptionShouldReturnNoMatchesFound()
+        {
+            var enricher = new MessageEnricher();
+
+            var worksheet = new SpreadsheetWorkbook();
+
+            var e = new SpreadsheetException("No matches found", new List<string>() { "C1", "C2", "C3" }, "token");
+
+            var actual = enricher.EnrichError(worksheet, e);
+
+            Assert.Equal("No match found for token [" + e.Token + "] related to formula '" + worksheet.GetFormulaName() + "'.", actual.Message);
+        }
+        [Fact]
+        public void ShouldReturnCircularReference()
+        {
+            var enricher = new MessageEnricher();
+
+            var worksheet = new SpreadsheetWorkbook();
+
+            var e = new SpreadsheetException("Circular Reference xxx", new List<string>() { "C1", "C2", "C3" }, "token");
+
+            var actual = enricher.EnrichError(worksheet, e);
+
+            Assert.Equal("Circular Reference in spreadsheet related to formula '" + worksheet.GetFormulaName() + "'. Cells: " + e.Cells, actual.Message);
+        }
+        [Fact]
+        public void ShouldReturnInvalidExpression()
+        {
+            var enricher = new MessageEnricher();
+
+            var worksheet = new SpreadsheetWorkbook();
+
+            var e = new ExpressionParseException("Object reference not set to an instance of an object");
+
+            var actual = enricher.EnrichError(worksheet, e);
+
+            Assert.Equal("Invalid expression found in tax formula [" + worksheet.GetFormulaName() + "]. Check that separators and delimiters use the English locale.", actual.Message);
         }
     }
 }
